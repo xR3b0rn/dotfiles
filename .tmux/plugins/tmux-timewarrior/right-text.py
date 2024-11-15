901701d1c72dc33bb18953ceb81936db449e4477
Julian Weiß <j.weiss@primes.de>
2024-11-15 09:37:11 +0100

    improved tmux timewarrior integration

---
+++ .tmux/plugins/tmux-timewarrior/right-text.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+from datetime import datetime
+from tasklib import TaskWarrior
+
+text = "#[bg=red]no active task"
+
+tw = TaskWarrior("~/.task")
+
+tasks = tw.tasks.pending().filter("+ACTIVE")
+if len(tasks) == 1:
+    task = tasks[0]
+    start = task["start"]
+    start = start.replace(tzinfo=None)
+    now = datetime.now()
+    now = now.replace(tzinfo=None)
+    delta = now - start
+    total_seconds = int(delta.total_seconds())
+    hours, remainder = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    project = task["project"]
+    desc = task["description"]
+    text = f"#[bg=green]{project}: {desc} {hours:02}:{minutes:02}:{seconds:02}"
+elif len(tasks) > 1:
+    text = "#[bg=red]more than one task active"
+
+print(text)
--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 pwn=$(dirname -- "${0}")
 ln -s $(cd . && pwd)/.zshrc ${HOME}
 ln -s $(cd . && pwd)/.tmux.conf ${HOME}
+ln -s $(cd . && pwd)/.tmux ${HOME}
 mkdir -p ${HOME}/.task
 ln -s $(cd . && pwd)/.task/hooks ${HOME}/.task
