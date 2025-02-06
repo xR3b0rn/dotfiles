f96336c4ec046d5371a183d9775c205ac5418ecb
Julian Weiß <j.weiss@primes.de>
2025-02-06 17:40:17 +0100

    Added installation step for timewarrior configuration

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ ln -s $(cd . && pwd)/.tmux.conf ${HOME}
 ln -s $(cd . && pwd)/.tmux ${HOME}
 mkdir -p ${HOME}/.task
 ln -s $(cd . && pwd)/.task/hooks ${HOME}/.task
+mkdir -p ${HOME}/.config
+ln -s $(cd . && pwd)/.config/timewarrior ${HOME}/.config/timewarrior
